b0a61b2bda2fdf1908033814d75285cc707939e5
sartra <strenshaw@gmail.com>
2016-04-27 21:12:01 -0400

    add css and js files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+      var sayMyName = function (name){
+        alert('My name is ' + name);  
+        // in console: 
+        // sayMyName('sarah');
+      }
+
+      var car = {
+        make: 'VW',
+        typcae: 'Polo',
+        color: 'blue',
+        seats: ['seat1', 'seat2'],
+        isTurnedOn: false,
+        turnOn: function () {
+          this.isTurnedOn = true; 
+        },
+        fly: function() {
+          console.log('fly');
+        },
+        switchCar: function(isOn) {
+          console.log('turn car '+ isOn)
+          if(isOn == true) {
+            this.isTurnedOn = true;
+          } else {
+            this.isTurnedOn = false;
+          }
+        }
+      };
